767fea6e68e73b5df38264fc589c51639f6d4d23
prokopenkotg <prokopenkotg@45155b4a-2f7b-5aa7-b1c3-0b815fc90ef3>
2010-11-09 15:24:02 +0000

    Реалізовано додавання товару

--- trunk/WebApplication8/Trade/AddProduct.aspx
+++ trunk/WebApplication8/Trade/AddProduct.aspx
@@ -1,6 +1,12 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Trade/Default.Master" AutoEventWireup="true" CodeBehind="AddProduct.aspx.cs" Inherits="WebApplication8.Trade.WebForm1" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-    
+    <style type="text/css">
+        .style1
+        {
+            width: 141px;
+        }
+    </style>
+
 
 
 
@@ -11,41 +17,106 @@
     
         <table style="height: 161px; width: 607px">
     <tr>
+    <td class="style1">
+        <asp:Label ID="Label2" runat="server" Text="Группа товара"></asp:Label>
+    </td>
     <td>
-        &nbsp;</td>
-    <td>
+        <asp:DropDownList ID="DDListGroup" runat="server" AutoPostBack="True" 
+            DataSourceID="SqlDataSourceGroup" DataTextField="Name" DataValueField="GroupID">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSourceGroup" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:ConnectionString22 %>" 
+            SelectCommand="SELECT [GroupID], [Name] FROM [GroupT]"></asp:SqlDataSource>
     </td>
     </tr>
         <tr>
-    <td>
+    <td class="style1">
+        <asp:Label ID="Label3" runat="server" Text="Фирма"></asp:Label>
     </td>
     <td>
+        <asp:DropDownList ID="DDListFirm" runat="server" 
+            DataSourceID="SqlDataSourceFirm" DataTextField="Name" DataValueField="FirmID">
+        </asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSourceFirm" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:ConnectionString22 %>" 
+            SelectCommand="SELECT [FirmID], [Name] FROM [Firm] WHERE ([GroupID] = @GroupID)">
+            <SelectParameters>
+                <asp:ControlParameter ControlID="DDListGroup" Name="GroupID" 
+                    PropertyName="SelectedValue" Type="Int32" />
+            </SelectParameters>
+        </asp:SqlDataSource>
     </td>
     </tr>
 
         <tr>
-    <td>
+    <td class="style1">
+
+        <asp:Label ID="Label7" runat="server" Text="Название"></asp:Label>
+
     </td>
     <td>
+        <asp:TextBox ID="TxtName" runat="server" Width="176px" CausesValidation="True"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+            ErrorMessage="* Необходимо заполнить" ControlToValidate="TxtName" 
+            SetFocusOnError="True" ValidationGroup="2"></asp:RequiredFieldValidator>
     </td>
     </tr>
 
         <tr>
-    <td>
+    <td class="style1">
+        <asp:Label ID="Label4" runat="server" Text="Описание"></asp:Label>
     </td>
     <td>
+        <asp:TextBox ID="TxtInfo" runat="server" Height="132px" TextMode="MultiLine" 
+            Width="452px"></asp:TextBox>
     </td>
     </tr>
 
         <tr>
-    <td>
+    <td class="style1">
+        <asp:Label ID="Label5" runat="server" Text="Цена"></asp:Label>
     </td>
     <td>
+        <asp:TextBox ID="TxtPrice" runat="server" Width="174px"></asp:TextBox>
+    </td>
+    </tr>
+
+    <tr>
+    <td >
+
+        <asp:Label ID="Label6" runat="server" Text="На  складе"></asp:Label>
+
+    </td>
+    <td>
+        <asp:TextBox ID="TxtInStock" runat="server" Width="172px"></asp:TextBox>
+    </td>
+    </tr>
+    <tr>
+    <td>
+
+        <asp:Label ID="Label8" runat="server" Text="Изображение"></asp:Label>
+
+    </td>
+    <td style="text-align: left">
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+    </td>
+    </tr>
+
+       <td >
+
+           &nbsp;</td>
+    <td style="text-align: right">
+        <asp:ImageButton ID="ImageButton4" runat="server" 
+            ImageUrl="~/Image/1289313975_Emulation.png" onclick="ImageButton4_Click" 
+            ValidationGroup="2" />
     </td>
     </tr>
 
 
     </table>
+
+<asp:Label ID="LabelMessage" runat="server" 
+    style="font-weight: 700; color: #00FF00; font-size: large" Visible="False"></asp:Label>
 
 </asp:Content>
 
--- trunk/WebApplication8/Trade/AddProduct.aspx.cs
+++ trunk/WebApplication8/Trade/AddProduct.aspx.cs
@@ -11,17 +11,16 @@ namespace WebApplication8.Trade
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        SqlConnection conection = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Warh\Documents\Visual Studio 2010\Projects\WebApplication8\WebApplication8\App_Data\webmarket.mdf;Integrated Security=True;User Instance=True");
+        SqlConnection conection = new SqlConnection(Varibles.ConectionString);
         SqlDataSource firmSource = new SqlDataSource();
         ProfileBase pb = new ProfileBase();
         ProfileInfoCollection pic = new ProfileInfoCollection();
-        String ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Warh\Documents\Visual Studio 2010\Projects\WebApplication8\WebApplication8\App_Data\webmarket.mdf;Integrated Security=True;User Instance=True";
-
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            LabelMessage.Visible = false;
-            TablePreView.Visible = false;
+            this.Title = "Добавление товара";
+            
         }
 
         public String GetFirstName()
@@ -40,32 +39,7 @@ namespace WebApplication8.Trade
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-                            //додавання товару до бази даних
-            try
-            {
-                conection.Open();
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = conection;
-                    byte[] ms = new byte[FileUpload1.PostedFile.InputStream.Length]; // створення змінної для зображення
-                    FileUpload1.PostedFile.InputStream.Read(ms, 0, ms.Length); //запис зображення до змінної
-
-
-
-                cmd.CommandText = "INSERT INTO Product (name,cost,info,firmID,image) VALUES ('" + TxtName.Text + "','" + TxtPrice.Text + "','" + TxtInfo.Text + "'," + DDListFirms.SelectedValue + ",@file)";
-                cmd.Parameters.AddWithValue("@file", ms);
-                cmd.ExecuteNonQuery();
-                LabelMessage.Text = "Товар "+TxtName.Text+"було додано успішно.";
-
-            }
-            catch (SqlException err)
-            {
-                TxtInfo.Text = err.Message;
-            }
-            finally
-            {
-                conection.Close();
-            
-            }
+           
             //if (FileUpload1.PostedFile.InputStream.Length == 0)
             //{
             //    Image1.ImageUrl = "no_entry.png";
@@ -99,6 +73,46 @@ namespace WebApplication8.Trade
             
 
            
+        }
+
+        protected void ImageButton4_Click(object sender, ImageClickEventArgs e)
+        {
+            LabelMessage.Visible = false;
+            //додавання товару до бази даних
+            try
+            {
+                conection.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conection;
+                byte[] ms = new byte[FileUpload1.PostedFile.InputStream.Length]; // створення змінної для зображення
+                FileUpload1.PostedFile.InputStream.Read(ms, 0, ms.Length); //запис зображення до змінної
+
+
+
+                cmd.CommandText = "INSERT INTO Product (name,cost,info,firmID,image) VALUES ('" + TxtName.Text + "','" + TxtPrice.Text + "','" + TxtInfo.Text + "'," + DDListFirm.SelectedValue + ",@file)";
+                cmd.Parameters.AddWithValue("@file", ms);
+                cmd.ExecuteNonQuery();
+                LabelMessage.Visible = true;
+                LabelMessage.Text = "Товар " + TxtName.Text + "було додано успішно.";
+
+            }
+            catch (SqlException err)
+            {
+                TxtInfo.Text = err.Message;
+            }
+            finally
+            {
+                conection.Close();
+
+            }
+
+
+            TxtInfo.Text = "";
+            TxtInStock.Text = "";
+            TxtName.Text = "";
+            TxtPrice.Text = "";
+
+
         }
     }
 }
